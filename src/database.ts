bf3e340b0a598026a74ad809bb8327e5c7baf9b4
Gary Ascuy <gary.ascuy@highjump.com>
2021-07-16 07:36:59 -0500

    Updating main module

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,11 @@
 import { Connection } from "@odata/server";
-import fs from "fs";
 import path from "path";
 
 export async function createDataFile(appDir: string): Promise<string> {
     const src = path.join(appDir, 'data', 'initial.sqlite');
-    const target = path.join(appDir, 'data', 'gary.sqlite');
-    if (!fs.existsSync(target)) await fs.promises.copyFile(src, target);
+    const target = path.join(appDir, 'data', 'current.sqlite');
+    // Todo: Generate new database without constraints
+    // if (!fs.existsSync(target)) await fs.promises.copyFile(src, target);
 
     return target;
 }
@@ -14,7 +14,8 @@ export async function importData(connection: Connection, path: string): Promise<
     const sqljsManager = connection.sqljsManager;
 
     try {
-        await sqljsManager.loadDatabase(path);
+        // Todo: Generate new database without constraints
+        // await sqljsManager.loadDatabase(path); // import has load database with constraints 
         const [result] = await sqljsManager.query("SELECT COUNT(*) as Count FROM Products");
         console.log('Products:', result);
     } catch (error) {
--- src/server.ts
+++ src/server.ts
@@ -1,69 +1,41 @@
-import { Connection, createTypedODataServer } from '@odata/server';
+import { createTypedODataServer } from '@odata/server';
+import cors from 'cors';
 import express from "express";
-import * as fs from 'fs';
 import * as path from 'path';
 import 'reflect-metadata';
+import { createDataFile, importData } from './database';
 import { AllEntries } from "./northwind";
 
-const appDir = path.resolve(__dirname, "../");
-const dataFile = path.join(appDir, 'data', 'current.sqlite');
-
 const run = async () => {
-
-    const port = parseInt(process.env.PORT || '50000', 10);
+    const host = process.env.SERVER_HOST || '0.0.0.0';
+    const port = parseInt(process.env.SERVER_PORT || '3666', 10);
+    const partialPath = process.env.SERVER_PORT || '/odata';
+    const appDir = path.resolve(__dirname, '../');
+    const corsOptions = { origin: "*", methods: "*", allowedHeaders: "*" };
 
     const server = await createTypedODataServer({
-      name: "default",
-      type: "sqljs",
-      synchronize: false, // don't create at start-up we use our own SQL database
-      // autoSave: true,
-      logging: true,
-      cache: true,
-      entities: [...AllEntries],
+        name: "default",
+        type: "sqljs",
+        synchronize: true, // don't create at start-up we use our own SQL database
+        autoSave: false,
+        logging: true,
+        cache: true,
+        entities: [...AllEntries],
     });
 
     server.namespace = "kendo_northwind_pg.Models"; // we can probably change this in the long run
-
     const connection = server.getConnection();
-
-    if (!fs.existsSync(dataFile)) createDataFile();
-
-    // Import dataFile
-    importData(connection, dataFile);
+    const dataFile = await createDataFile(appDir);
+    await importData(connection, dataFile);
 
     const app = express();
-
+    app.use(cors(corsOptions));
     app.set('trust proxy', true);
+    app.use(partialPath, server.create(partialPath));
 
-    app.use(server.create());
-
-    app.listen(port, () => {
-        console.log(`server started at ${port}`);
+    app.listen(port, host, () => {
+        console.log(`server started at ${host}:${port}/${partialPath}`);
     });
-};
-
-const createDataFile = () => {
-    const src = path.join(appDir, 'data', 'initial.sqlite');
-    const target = path.join(appDir, 'data', 'current.sqlite');
-    fs.copyFileSync(src, target);
-};
-
-const importData = async (connection: Connection, path: string) => {
-    const sqljsManager = connection.sqljsManager;
-
-    try {
-        await sqljsManager.loadDatabase(path);
-        // const resultAfterLoad = await sqljsManager.query("SELECT * FROM Customers");
-        // console.log('resultAfterLoad', resultAfterLoad);
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-const exportData = async (connection: Connection, path: string) => {
-    const sqljsManager = connection.sqljsManager;
-    sqljsManager.saveDatabase(path);
-    console.info('saved into', path);
 };
 
 if (require.main == module) {
