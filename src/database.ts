e598de488d45caa38988ca5747e4ebb93d8d7da0
Gary Ascuy <gary.ascuy@highjump.com>
2021-07-16 22:05:29 -0500

    Fixing issues with server

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,11 @@
-import { Connection } from "@odata/server";
-import path from "path";
+import { Connection } from '@odata/server';
+import fs from 'fs';
+import path from 'path';
 
 export async function createDataFile(appDir: string): Promise<string> {
     const src = path.join(appDir, 'data', 'initial.sqlite');
     const target = path.join(appDir, 'data', 'current.sqlite');
-    // Todo: Generate new database without constraints
-    // if (!fs.existsSync(target)) await fs.promises.copyFile(src, target);
+    if (!fs.existsSync(target)) await fs.promises.copyFile(src, target);
 
     return target;
 }
@@ -14,9 +14,8 @@ export async function importData(connection: Connection, path: string): Promise<
     const sqljsManager = connection.sqljsManager;
 
     try {
-        // Todo: Generate new database without constraints
-        // await sqljsManager.loadDatabase(path); // import has load database with constraints 
-        const [result] = await sqljsManager.query("SELECT COUNT(*) as Count FROM Products");
+        await sqljsManager.loadDatabase(path);
+        const [result] = await sqljsManager.query('SELECT COUNT(*) as Count FROM Products');
         console.log('Products:', result);
     } catch (error) {
         console.error(error);
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,31 @@
+#!/usr/bin/env node
+
 import { createTypedODataServer } from '@odata/server';
 import cors from 'cors';
-import express from "express";
+import express from 'express';
 import * as path from 'path';
 import 'reflect-metadata';
 import { createDataFile, importData } from './database';
-import { AllEntries } from "./northwind";
+import { AllEntries } from './northwind';
 
 const run = async () => {
     const host = process.env.SERVER_HOST || '0.0.0.0';
     const port = parseInt(process.env.SERVER_PORT || '3666', 10);
     const partialPath = process.env.SERVER_PORT || '/odata';
     const appDir = path.resolve(__dirname, '../');
-    const corsOptions = { origin: "*", methods: "*", allowedHeaders: "*" };
+    const corsOptions = { origin: '*', methods: '*', allowedHeaders: '*' };
 
     const server = await createTypedODataServer({
-        name: "default",
-        type: "sqljs",
-        synchronize: true, // don't create at start-up we use our own SQL database
+        name: 'default',
+        type: 'sqljs',
+        synchronize: false, // don't create at start-up we use our own SQL database
         autoSave: false,
         logging: true,
         cache: true,
         entities: [...AllEntries],
     });
 
-    server.namespace = "kendo_northwind_pg.Models"; // we can probably change this in the long run
+    server.namespace = 'kendo_northwind_pg.Models'; // we can probably change this in the long run
     const connection = server.getConnection();
     const dataFile = await createDataFile(appDir);
     await importData(connection, dataFile);
@@ -31,7 +33,7 @@ const run = async () => {
     const app = express();
     app.use(cors(corsOptions));
     app.set('trust proxy', true);
-    app.use(partialPath, server.create(partialPath));
+    app.use(partialPath, server.create());
 
     app.listen(port, host, () => {
         console.log(`server started at ${host}:${port}${partialPath}`);
